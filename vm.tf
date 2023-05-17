6434dcbdc350aecaac6404e13ecfd11a53b7e50b
CyberGrizz88 <cybergrizz88@gmail.com>
2023-05-17 02:51:28 -0400

    Updating code based on GCP documentation

--- vm.tf
+++ vm.tf
@@ -1,24 +1,16 @@
 resource "google_compute_instance" "default" {
-  name         = "test"
-  machine_type = "e2-medium"
+  name         = "my-vm"
+  machine_type = "n1-standard-1"
   zone         = "us-central1-a"
-
-  network_interface {
-    network = "default"
-
-    access_config {
-      // Ephemeral public IP
-    }
-  }
-
-  tags = ["foo", "bar"]
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-11"
-      labels = {
-        my_label = "value"
-      }
+      image = "ubuntu-minimal-2210-kinetic-amd64-v20230126"
     }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
   }
 }
