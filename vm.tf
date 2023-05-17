d09d43ee02650dc001cf9429f99359570333187e
CyberGrizz88 <cybergrizz88@gmail.com>
2023-05-17 02:55:57 -0400

    adding template for boot script

--- vm.tf
+++ vm.tf
@@ -3,6 +3,11 @@ resource "google_compute_instance" "custom_subnet" {
   tags         = ["allow-ssh"]
   zone         = "europe-west1-b"
   machine_type = "e2-small"
+  metadata = {
+    # Shuts down Apache server
+    shutdown-script = file("${path.module}/shutdown-script.sh")
+  }
+
   network_interface {
     network    = google_compute_network.custom.id
     subnetwork = google_compute_subnetwork.custom.id
